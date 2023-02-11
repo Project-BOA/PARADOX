81aa27b227d1aa61116db438fe53309c06123867
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2023-02-11 17:43:46 +0000

    hide title and titlebar and add profile and home fragments with viewmodels

--- app/src/main/java/team/boa/paradox/MainActivity.kt
+++ app/src/main/java/team/boa/paradox/MainActivity.kt
@@ -2,10 +2,18 @@ package team.boa.paradox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Hide title and title bar
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportActionBar?.hide();
+
+
+
         setContentView(R.layout.activity_main)
     }
 }
