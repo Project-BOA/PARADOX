ada2f1a69e80af908a05b7db0a51759b830cdc06
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2023-02-11 20:56:00 +0000

    add database connection, loading animation and input validation

--- app/src/main/java/team/boa/paradox/WelcomeActivity.kt
+++ app/src/main/java/team/boa/paradox/WelcomeActivity.kt
@@ -2,12 +2,16 @@ package team.boa.paradox
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
@@ -17,18 +21,48 @@ class WelcomeActivity : AppCompatActivity() {
 
         // get submit components from view
         val submitButton : Button = findViewById(R.id.button_puzzle_id)
-        val puzzle_id_input: EditText = findViewById(R.id.edit_text_puzzle_id)
+        val puzzleIdInput: EditText = findViewById(R.id.edit_text_puzzle_id)
+        val loader = findViewById<ProgressBar>(R.id.loading_submit)
 
-        // intent to pass data back to main activity
-        val data = Intent()
+        submitButton.setOnClickListener {
+            loader.isVisible = true; // enable loading animation
 
-        submitButton.setOnClickListener{
+            // clean input
+            val cleanedInputString = cleanPuzzleId(puzzleIdInput.text.trim().toString())
 
-            var cleaned_input_string = puzzle_id_input.text.trim().toString();
-
-            data.putExtra("puzzle_id", cleaned_input_string)
-            setResult(RESULT_OK, data)
-            finish()
+            // validate puzzle_id
+            validatePuzzleId(cleanedInputString, loader)
         }
     }
+
+    private fun validatePuzzleId(puzzleId:String, loader: ProgressBar) {
+        database = FirebaseDatabase.getInstance().getReference("answers")
+        database.child(puzzleId).get().addOnSuccessListener {
+            if(it.exists()){
+                // intent to pass data back to main activity
+                val data = Intent()
+                data.putExtra("puzzle_id", puzzleId)
+                setResult(RESULT_OK, data)
+                finish()
+            }
+            else{
+                loader.isVisible = false; // enable loading animation
+                Toast.makeText(this,"Puzzle does not exist", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener(){
+            loader.isVisible = false; // enable loading animation
+            Toast.makeText(this,"Error Checking Puzzle Id", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun cleanPuzzleId(inputString: String): String {
+
+        // codes only contain uppercase characters
+        var cleaned = inputString.uppercase()
+        // only allow numbers and capital letters
+        cleaned = inputString.replace("[^0-9A-Z]".toRegex(), "")
+
+        return cleaned
+    }
+
 }
